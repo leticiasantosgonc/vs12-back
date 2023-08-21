cf833319656b23cc673b328e229f3091a21755d4
Letícia Santos <leticiasantosgonc@gmail.com>
2023-08-21 10:24:45 -0300

    corrigindo metodo de criptografia

--- modulo-03-3-springsecurity/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/security/SecurityConfiguration.java
+++ modulo-03-3-springsecurity/pessoa-api/src/main/java/br/com/dbc/vemser/pessoaapi/security/SecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -78,6 +79,6 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder passwordEncoder(){
-        return new Pbkdf2PasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
