72fc9d69a8570dc2f05c5eaa57afa05667f51358
Letícia Santos <leticiasantosgonc@gmail.com>
2023-08-30 09:12:04 -0300

    task 01 kafka

---
+++ modulo-04-3-kafka/produtor-consumidor/src/main/java/br/com/dbc/vemser/produtorconsumidor/controller/ProdutorController.java
@@ -0,0 +1,21 @@
+package br.com.dbc.vemser.produtorconsumidor.controller;
+
+
+import br.com.dbc.vemser.produtorconsumidor.service.ProdutorService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+@RequiredArgsConstructor
+public class ProdutorController {
+    private final ProdutorService produtorService;
+
+    @PostMapping("/enviar-mensagem")
+    public void enviarMensagem(String mensagem) {
+        produtorService.enviarMensagem(mensagem);
+    }
+}
+
